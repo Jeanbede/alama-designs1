e22030220e1c6b88f40818dcf2660278042480bc
Jeanbede <njeanbeda@gmail.com>
2021-01-05 02:17:42 +0300

    adding files to webpage

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 
- // Submit form with id function.
+ 
 function submit_by_id() {
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
-    if (validation()) // Calling validation function
+    if (validation()) 
     {
-    document.getElementById("form_id").submit(); //form submission
+    document.getElementById("form_id").submit(); 
     alert(" Name : " + name + " n Email : " + email + " n Form Id : " + document.getElementById("form_id").getAttribute("id") + "nn Form Submitted Successfully......");
     }
     }
     
        
-    // Name and Email validation Function.
+   
     function validation() {
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
